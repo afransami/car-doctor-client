41f561d0602cebb413755f5160d0d9d3ce0711b7
afransami <afran.sami007@gmail.com>
2023-05-10 23:58:19 +0600

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 import loginBanner from '../../assets/images/login/login.svg'
 
 const Login = () => {
+    const handleLogin= event =>{
+        event.preventDefault();
+        form= event.target;
+        email= form.target.value;
+        password = form.target.value;
+        
+    }
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <div className="text-center lg:text-left w-1/2">          
+        <div className="w-1/2">          
           <img src={loginBanner} alt="" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ml-20">
         <h1 className="text-4xl p-5 font-bold">Login!</h1>
+          <Form onSubmit={handleLogin}>
           <div className="card-body">
             <div className="form-control">
               <label className="label">
@@ -37,11 +45,12 @@ const Login = () => {
                 </a>
               </label>
             </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+            <div className="form-control mt-6">              
+              <input className="btn btn-primary"  type= "submit" value="Login" />
               <p className="text-center mt-5 font-semibold">New to car doctor? Please <Link>register</Link></p>
             </div>
           </div>
+          </Form>
         </div>
       </div>
     </div>
