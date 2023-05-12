c3e3f1ac8482e99aba097e0ec9ac772858b553be
afransami <afran.sami007@gmail.com>
2023-05-12 12:58:51 +0600

    booking card displaying

--- src/pages/BookingCard/BookingCard.jsx
+++ src/pages/BookingCard/BookingCard.jsx
@@ -1,74 +1,50 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
-
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import BookingRow from "./BookingRow";
 
 const BookingCard = () => {
-    
-    const {user}= useContext(AuthContext)
-    const [booking, setBooking]=useState([])
+  const { user } = useContext(AuthContext);
+  const [bookings, setBooking] = useState([]);
 
-    const url = `http://localhost:5000/checkout?email=${user?.email}`
-    useEffect(()=>{
-     fetch (url)
-    .then (res=> res.json())
-    .then (data=> console.log(data))
-    },[])
-    
+  const url = `http://localhost:5000/checkout?email=${user?.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBooking(data));
+  }, []);
 
-    return (
-        <div className="overflow-x-auto w-full">
-  <table className="table w-full">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div>
-          </div>
-        </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br/>
-          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-        </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>  
-    </tbody>
-
-    
-  </table>
-</div>
-    );
+  return (
+    <div className="overflow-x-auto w-full">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Name</th>
+            <th>Service</th>
+            <th>Date</th>
+            <th>Price</th>            
+            <th>Details</th>            
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            bookings.map(booking=><BookingRow
+            key={booking._id}
+            booking={booking}
+            >
+            </BookingRow>          
+        )}
+          
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BookingCard;
